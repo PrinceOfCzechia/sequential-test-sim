e4edc28783f126c83e582f0c5f5e094ea6fa7ded
Petr Král <pkral2000@gmail.com>
2024-03-11 22:29:20 +0100

    added adn tested function to generate X_i

--- sim.py
+++ sim.py
@@ -1,24 +1,31 @@
 import numpy as np
 
 '''
-dal bude platit
-X1, X2, ... nahodne veliciny splnujici
+assume that
+X1, X2, ... are random variables such that
     P(Xi = 1) = p
     P(Xi = 0) = 1 - p
+we aim to test the hypotheses
 H0: p = p0
 H1: p = p1
 0 < p0 < p1 < 1
 '''
 
+
 def gen_X( p ):
-    return( np.random.uniform( 0.0, 1.0, 1 ) )
+    '''
+    draw Xi from the pre-specified distribution
+    '''
+    r = np.random.uniform( 0.0, 1.0, 1 )
+    return( r < p )
+
 
 '''
-1) Test s pevnym rozsahem vyberu
+1) Test with a fixed sample size
 '''
 n = 100
 alpha = 0.05
 x = np.random.uniform( 0.0, 1.0, n )
 
 Sn = np.sum( x )
-print( Sn )
+# print( Sn )

e21d3e91afc38a3943b43374c627c035c75dac27
Petr Král <pkral2000@gmail.com>
2024-03-12 16:42:24 +0100

    fixed deprecation error, improved on test1

--- sim.py
+++ sim.py
@@ -1,8 +1,11 @@
 import numpy as np
+import scipy.stats as stats
+from scipy.stats import binom
+from scipy.stats import bernoulli # TODO: rethink and remove
 
 '''
 assume that
-X1, X2, ... are random variables such that
+X1, X2, ... be iid random variables with Xi ~ Alt(p), i.e.
     P(Xi = 1) = p
     P(Xi = 0) = 1 - p
 we aim to test the hypotheses
@@ -15,6 +18,7 @@ H1: p = p1
 def gen_X( p ):
     '''
     draw Xi from the pre-specified distribution
+    with the parameter value set to 'p'
     '''
     r = np.random.uniform( 0.0, 1.0, 1 )
     return( r < p )
@@ -25,7 +29,49 @@ def gen_X( p ):
 '''
 n = 100
 alpha = 0.05
-x = np.random.uniform( 0.0, 1.0, n )
+p = 0.30
+p0 = 0.30
+p1 = 0.70
+x = np.empty( n )
 
-Sn = np.sum( x )
-# print( Sn )
+
+def eval_k( n, p0, alpha ):
+    return binom.ppf( 1-alpha, n, p0 )
+
+def test1( k ):
+    x = np.array( [ gen_X( p ) for i in range( n ) ] )
+    Sn = np.sum( x )
+
+    # print( 'Sn = ', Sn )
+    # print( 'k = ', k)
+
+    return( Sn > k)
+
+lvl = 0
+for i in range( 1000 ):
+    H = test1( eval_k( n, p0, alpha ) )
+    if( H ): lvl += 1
+
+print( lvl/1000 )
+
+
+'''
+2) Two-stage test
+'''
+n1 = 50
+n2 = 50
+
+
+'''
+3) 
+'''
+
+
+'''
+4) 
+'''
+
+
+'''
+5) 
+'''
